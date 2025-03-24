7c5d533e26a367475fdd6a8d3781f0e757215813
JeanBizeul <jean.bizeul@epitech.eu>
2025-03-24 17:47:53 +0100

    ✨ Add sound data class

---
+++ include/DataStructures/Sound.hpp
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2025
+** Arcade-Headers
+** File description:
+** Sound
+*/
+
+#pragma once
+#include <string>
+
+class Sound {
+    public:
+        enum State {
+            PLAY,
+            STOP,
+            LOOP
+        };
+        Sound(
+            std::string filePath,
+            Sound::State state,
+            std::string id,
+            bool unique = true
+        );
+};
