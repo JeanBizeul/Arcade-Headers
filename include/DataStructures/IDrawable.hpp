08323481ca8e83defbb1f56d8a0b016852c658e7
Arthuryan Loheac <arthuryan.loheac-de-sainte-marie@epitech.eu>
2025-03-25 10:18:36 +0100

    ✨ Extend IDrawable interface with scale, rotation, color, and position methods

--- include/DataStructures/IDrawable.hpp
+++ include/DataStructures/IDrawable.hpp
@@ -20,4 +20,15 @@ class IDrawable
             std::tuple<int, int, int, int> GUI_Color,
             std::pair<int, int> position
         ) = delete;
+
+        virtual std::pair<float, float> getScale() const = 0;
+        virtual float getRotation() const = 0;
+        virtual std::pair<CLI_Color, CLI_Color> getCLI_Color() const = 0;
+        virtual std::tuple<int, int, int, int> getGUI_Color() const = 0;
+        virtual std::pair<int, int> getPosition() const = 0;
+        virtual void setScale(std::pair<float, float> scale) = 0;
+        virtual void setRotation(float rotation) = 0;
+        virtual void setCLI_Color(std::pair<CLI_Color, CLI_Color> CLI_Color) = 0;
+        virtual void setGUI_Color(std::tuple<int, int, int, int> GUI_Color) = 0;
+        virtual void setPosition(std::pair<int, int> position) = 0;
 };
